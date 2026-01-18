9b108f1f596b161fa293464225749711eae468bd
kunal15cr <kunal15cr@gmail.com>
2026-01-19 02:34:13 +0530

    utility functions for hashing passwords and checking password is correct

--- utils.py
+++ utils.py
@@ -1,1 +1,17 @@
 from passlib.context import CryptContext
+
+paw_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+fake_user_db = {
+    "johndoe": {
+       "username": "johndoe",
+         "hashed_password": paw_context.hash("secret")
+         }
+}
+
+def get_user(username: str):
+    user = fake_user_db.get(username)
+    return user
+
+def verify_password(plain_password: str, hashed_password: str):
+    return paw_context.verify(plain_password, hashed_password)
